af238726159cd23558dae644e6917409846613bd
abhi6004 <abhiradadiya30012004@gmail.com>
2023-03-31 09:16:55 +0530

    new code add

--- Testimonial-Silder/src/components/Card.js
+++ Testimonial-Silder/src/components/Card.js
@@ -1,38 +1,46 @@
-import React from 'react'
-import {FaQuoteLeft, FaQuoteRight} from 'react-icons/fa';
+import React from "react";
+import {FaQuoteLeft,FaQuoteRight} from 'react-icons/fa';
 
 
-const Card = (props) => {
+function Card(props) {
+
     let review = props.review;
-  return (
-    <div className='flex flex-col md:relative'>
-      <div className='absolute top-[-7rem] z-[10] mx-auto'>
-        <img 
-        className="aspect-square rounded-full w-[140px] h-[140px] z-[25]"
-        src={review.image}/>
-        <div className='w-[140px] h-[140px] bg-violet-500 rounded-full absolute
-         top-[-6px] z-[-10] left-[10px]'></div>
-      </div>
-      
-      <div className='text-center mt-7'>
-        <p className='tracking-wider font-bold text-2xl capitalize'>{review.name}</p>
-        <p className='text-violet-300 uppercase text-sm'>{review.job}</p>
-      </div>
 
+    return(
+        <div className="flex flex-col md:relative">
+            <div className="absolute top-[-7rem] z-[10] mx-auto">
+                <img
+                className="aspect-square rounded-full w-[140px] h-[140px] z-[25]" 
+                src={review.image}/>
+                <div className="w-[140px] h-[140px] bg-violet-500 rounded-full absolute top-[-6px] z-[20] left-[10px]"></div>
+            </div>
 
-      <div className='text-violet-400 mx-auto mt-5'>
-        <FaQuoteLeft/>
-      </div>
+            <div className="text-center mt-7">
+                <p className="font-bold text-2xl capital">{review.name}</p>
+            </div>
 
-      <div className='text-center mt-4 text-slate-500'>
-        {review.text}
-      </div>
+            <div className="text-center mt-7">
+                <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+            </div>
 
-      <div  className='text-violet-400 mx-auto mt-5'>
-        <FaQuoteRight/>
-      </div>
-    </div>
-  )
+            <div className="text-center mt-7">
+                <p className="text-violet-300 uppercase text-sm">{review.job}</p>
+            </div>
+
+            <div className="text-violet-400 mx-auto mt-5">
+                <FaQuoteLeft/>
+            </div>
+
+            <div className="text-center mt-4 text-slate-500">
+                {review.text}
+            </div>
+
+            <div className="text-violet-400 mx-auto mt-5">
+                <FaQuoteRight/>
+            </div>
+
+        </div>
+    );
 }
 
-export default Card
+export default Card;
---
+++ Testimonial-Silder/src/components/Testimonial.js
@@ -0,0 +1,66 @@
+import React, { useState } from "react";
+import Card from "./Card";
+import {FiChevronLeft,FiChevronRight} from 'react-icons/fi';
+
+function Testimonial(props) {
+
+    let reviews = props.reviews;
+    const[index,setIndex] = useState(0);
+
+    function leftShiftHandler() {
+        if(index - 1 < 0) {
+            setIndex(reviews.langth - 1);
+        }
+
+        else{
+            setIndex(index - 1);
+        }
+
+    } 
+
+    function RightShiftHandler() {
+        if(index + 1 >= reviews.langth) {
+            setIndex(0);
+        }
+
+        else{
+            setIndex(index + 1);
+        }
+    }
+
+    function SurpriceHandler() {
+        const rendomIndex = Math.floor(Math.random()*reviews.langth);
+        setIndex(rendomIndex);
+    }
+
+    return(
+        <div>
+            <Card review = {reviews[index]}/>
+
+            <div className="flex text-3xl mt-10 gap-3 text-violet-400 font-bold mx-auto">
+                <button 
+                onClick={leftShiftHandler}
+                className="cursor-pointer hover:text-violet-500">
+                    <FiChevronLeft/>
+                </button>
+
+                <button 
+                onClick={RightShiftHandler}
+                className="cursor-pointer hover:text-violet-500">
+                    <FiChevronRight/>
+                </button>
+            </div>
+
+            <div>
+                <button 
+                onClick={SurpriceHandler}
+                className="bg-violet-400 hover:bg-violet-500 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg">
+                    Surprice Me
+                </button>
+            </div>
+        </div>
+        
+    );
+}
+
+export default Testimonial;
